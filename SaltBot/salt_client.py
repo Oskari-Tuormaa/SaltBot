eb036e3dc9b299d5889b45b8db614afe0cd1d031
Oskari Tuormaa <oskaritu@gmail.com>
2020-05-29 00:28:45 +0200

    Added sneaky, and multi command functionality

--- SaltBot/salt_client.py
+++ SaltBot/salt_client.py
@@ -14,31 +14,36 @@ class SaltClient(discord.Client):
 
     async def on_message(self, message):
         if (message.content[0] == '!'):
-            cmd = message.content[1:]
-            await self.dispatcher(cmd, message)
+            # Check for sneaky
+            cmds = message.content[1:].split(" ")
+            if "-sneaky" in cmds:
+                cmds.remove("-sneaky")
+                await message.delete()
+
+            await self.dispatcher(cmds, message)
             
-    async def dispatcher(self, cmd, message):
-        if os.path.isfile(f'./sound_bytes/memes/{cmd}.mp3'):
-            if message.author.voice == None:
-                await message.channel.send("You're not in a channel")
-                return
+    async def dispatcher(self, cmds, message):
+        for cmd in cmds:
+            if os.path.isfile(f'./sound_bytes/memes/{cmd}.mp3'):
+                if message.author.voice == None:
+                    await message.channel.send("You're not in a channel")
+                    return
 
-            if message.guild.name not in players or players[message.guild.name] == None:
-                players[message.guild.name] = Player(message.author.voice.channel, players, self.loop)
+                if message.guild.name not in players or players[message.guild.name] == None:
+                    players[message.guild.name] = Player(message.author.voice.channel, players, self.loop)
 
-            players[message.guild.name].add_to_queue(cmd, None)
+                players[message.guild.name].add_to_queue(cmd, None)
 
-        elif cmd == "help":
-            await message.channel.send("{} is a big dumb baby".format(message.author.name))
+            elif cmd == "help":
+                await message.channel.send("{} is a big dumb baby".format(message.author.name))
 
-        elif cmd == "skrid":
-            if message.guild.name in players:
-                print(" " * 500, end='\r')
-                print(", ".join(players), end='\r')
-                await players[message.guild.name].channel.disconnect()
+            elif cmd == "skrid":
+                if message.guild.name in players:
+                    print(" " * 500, end='\r')
+                    print(", ".join(players), end='\r')
+                    await players[message.guild.name].channel.disconnect()
+                else:
+                    await message.channel.send("I'm not even in a channel")
+
             else:
-                await message.channel.send("I'm not even in a channel")
-
-
-        else:
-            await message.channel.send(random.choice(UNKNOWN_CMD))
+                await message.channel.send("{}: {}".format(cmd, random.choice(UNKNOWN_CMD)))
