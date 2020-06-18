66e3ae44ff0a20d25394ddfc4f67ac4d9a867b2e
Oskari Tuormaa <oskaritu@gmail.com>
2020-06-19 00:15:29 +0200

    Cleaned up echo statement

--- SaltBot/sound_bytes/normalize_memes.sh
+++ SaltBot/sound_bytes/normalize_memes.sh
@@ -8,9 +8,6 @@ for f in $DIR/memes_raw/*.mp3
 do
   norm_gain=$(ffmpeg -i $f -af 'volumedetect' -f null /dev/null 2>&1 | grep -P '(?<=max_volume: ).*(?= dB)' -o)
   norm_gain=$(echo $norm_gain | sed 's/-//g')
-  echo -n $f
-  echo -en "\t:\t"
-  echo -n $norm_gain
-  echo " dB"
+  echo -en $f "\t:\t" $norm_gain " dB\n"
   ffmpeg -i $f -filter:a "volume=${norm_gain}dB" ${f/#$DIR\/memes_raw\//$DIR\/memes\/} -y -loglevel 8
 done
