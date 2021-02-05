49f4ec2a4807c89bf905d83727283fae71dc554f
Oskari Tuormaa <oskaritu@gmail.com>
2021-02-05 01:09:53 +0100

    Replaced auto_update with run script

--- auto_update.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-while sleep $1
-do
-	git pull | grep "up to date" || bash SaltBot/sound_bytes/normalize_memes.sh
-done
---
+++ run.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+# Kill background processes on exit
+trap "kill 0" EXIT
+
+# Function for starting server and saving PID
+START_SERVER() {
+	# Start server
+	python3 SaltBot/main.py &
+
+	# Get server PID
+	SERVER_PID=$(ps aux |
+		grep "python3 SaltBot/main.py" |
+		grep -oP "^.*?\K\d+" |
+		head -1)
+}
+
+# Initial startup of server
+START_SERVER
+
+
+while sleep $1; do
+	# Pull repo, and set PULLED to reflect whether we pulled or not
+	[[ -z "$(git pull | grep 'up to date')" ]] && PULLED=1 || PULLED=0
+
+	if [[ PULLED == 1 ]]; then
+		# Kill server
+		kill $SERVER_PID
+
+		# Normalize memes
+		bash SaltBot/sound_bytes/normalize_memes.sh
+
+		# Start server again
+		START_SERVER
+	fi
+done
+
+wait
