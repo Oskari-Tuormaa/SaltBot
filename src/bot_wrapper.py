065aa6c248e782b4b88a7774c77970a18e7a3d11
Oskari Tuormaa <oskaritu@gmail.com>
2021-12-13 05:41:55 +0100

    Made Client run in parallel

--- src/bot_wrapper.py
+++ src/bot_wrapper.py
@@ -1,5 +1,6 @@
 import asyncio
 import logging
+from multiprocessing import Process
 from time import sleep
 
 import git
@@ -13,28 +14,35 @@ def bot_wrapper(token: str):
     normalize_audio_clips()
 
     client = SaltClient()
-    client.run(token)
+
+    loop = asyncio.get_event_loop()
+    loop.create_task(client.start(token))
+    p = Process(target=loop.run_forever)
+    p.start()
 
     repo = git.Repo(root_dir())
 
-    while True:
-        # Check once a minute
-        sleep(60)
+    try:
+        while True:
+            # Check once a minute
+            sleep(1)
 
-        # Check for new commits on origin
-        branch = repo.active_branch
-        n_new = len(list(repo.iter_commits(f"{branch}..origin/{branch}")))
+            # Check for new commits on origin
+            branch = repo.active_branch
+            n_new = len(list(repo.iter_commits(f"{branch}..origin/{branch}")))
 
-        # If new commits detected
-        if n_new > 0:
-            logging.info(f"Pulling from {branch}")
-            repo.remotes.origin.pull()
+            # If new commits detected
+            if n_new > 0:
+                logging.info(f"Pulling from {branch}")
+                repo.remotes.origin.pull()
 
-            # Renormalize audio
-            normalize_audio_clips()
+                # Renormalize audio
+                normalize_audio_clips()
 
-            # Restart client
-            task = asyncio.get_running_loop().create_task(client.close())
-            asyncio.ensure_future(task)
+                # Restart client
+                task = asyncio.get_running_loop().create_task(client.close())
+                asyncio.ensure_future(task)
 
-            client.run(token)
+                client.run(token)
+    except (KeyboardInterrupt, InterruptedError):
+        p.terminate()
